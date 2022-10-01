f2270510d7116388fa37a80478dcdc5dd6f6550b
yzzlqyxc <yzzlqyxc@163.com>
2022-10-01 19:01:36 +0800

    check bug

--- kernel/proc.c
+++ kernel/proc.c
@@ -132,6 +132,14 @@ found:
     return 0;
   }
 
+  // Allocate a usyscall page
+  if((p->usyscall = (struct usyscall*)kalloc()) == 0){
+    freeproc(p);
+    release(&p->lock);
+    return 0;
+  }
+  p->usyscall->pid = p->pid;
+
   // An empty user page table.
   p->pagetable = proc_pagetable(p);
   if(p->pagetable == 0){
@@ -198,6 +206,12 @@ proc_pagetable(struct proc *p)
   if(mappages(pagetable, TRAPFRAME, PGSIZE,
               (uint64)(p->trapframe), PTE_R | PTE_W) < 0){
     uvmunmap(pagetable, TRAMPOLINE, 1, 0);
+    uvmfree(pagetable, 0);
+    return 0;
+  }
+
+  if(mappages(pagetable, USYSCALL, PGSIZE, 
+            (uint64)(p->usyscall), PTE_R) < 0) {
     uvmfree(pagetable, 0);
     return 0;
   }
